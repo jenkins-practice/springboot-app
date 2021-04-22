b726a637074f133f6538f697f9b5a64413c63d2b
AshanChandrasiri <ashanchandrasiri1@gmail.com>
2021-04-23 03:59:48 +0530

    initial commit

---
+++ src/main/java/com/example/jenkins/demo/controller/ThemeParkRideController.java
@@ -0,0 +1,18 @@
+package com.example.jenkins.demo.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class ThemeParkRideController {
+
+    @GetMapping(value = "/ride/{id}")
+    public ResponseEntity<String> getRide(@PathVariable long id){
+        return ResponseEntity.ok().body("hello rider");
+    }
+
+}
